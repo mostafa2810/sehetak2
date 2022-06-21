90c917720b1587bbca4c17861b85dd5920da510b
unknown <mostafa.helaly2810@gmail.com>
2022-06-21 19:06:54 +0200

    mostafa update

--- lib/services/global_method.dart
+++ lib/services/global_method.dart
@@ -9,14 +9,7 @@ class GlobalMethods {
           return AlertDialog(
             title: Row(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 6.0),
-                  child: Image.network(
-                    'https://image.flaticon.com/icons/png/128/564/564619.png',
-                    height: 20,
-                    width: 20,
-                  ),
-                ),
+              
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(title),
